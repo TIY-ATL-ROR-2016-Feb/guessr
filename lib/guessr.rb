7bf4019f5191e492a092c85dbf1e247536bf03e9
Brit Butler <redline6561@gmail.com>
2016-03-24 11:41:41 -0400

    Add a first pass at a number guessing game with models.

--- lib/guessr.rb
+++ lib/guessr.rb
@@ -8,5 +8,75 @@ require "guessr/score"
 
 module Guessr
   class App
+    def prompt(message, regex)
+      puts message
+      input = gets.chomp
+      until input =~ regex
+        puts "Sorry, that is invalid input. Please try again."
+        puts message
+        input = gets.chomp
+      end
+      input
+    end
+
+    def login
+      name = prompt("What's your name, player?", /^[a-z]{4,20}$/i)
+      Player.find_or_create_by(name: name)
+    end
+
+    def play(game)
+      until game.finished?
+        guess = game.take_turn
+        exit if guess == "Q"
+        game.update(last_guess: guess.to_i)
+        game.update(finished: true) if game.answer == guess.to_i
+      end
+    end
+
+    def new_game(player)
+      game = Game.create(answer: rand(100), player_id: player.id)
+      play(game)
+    end
+
+    def resume_game(player)
+      games = Game.where(player_id: player.id, finished: false)
+      games.each do |game|
+        puts "Game (id: #{game.id}, last guess: #{game.last_guess})"
+      end
+      puts "Please tell us the Game ID you'd like to resume."
+      game_id = gets.chomp.to_i
+      game = Game.find(game_id)
+      play(game)
+    end
+
+    def view_scores
+    end
+
+    def choose_game_mode(player)
+      puts "\n ** What would you like to do? ** \n"
+      choice = prompt("1) Play New Game\n2) Resume Game\n3) View Scores\n4) Quit\n", /^[1,2,3,4]$/)
+      case choice.to_i
+      when 1
+        new_game(player)
+      when 2
+        resume_game(player)
+      when 3
+        view_scores
+      when 4
+        puts "Goodbye. Thanks for wasting time with me."
+        exit
+      end
+    end
+
+    def run
+      player = login
+      while true
+        choose_game_mode(player)
+      end
+    end
   end
 end
+
+app = Guessr::App.new
+app.run
+
--- lib/guessr/game.rb
+++ lib/guessr/game.rb
@@ -1,4 +1,17 @@
 module Guessr
   class Game < ActiveRecord::Base
+    def low_or_high
+      self.last_guess < self.answer ? "low" : "high"
+    end
+
+    def take_turn
+      puts "To quit at any time, just type 'Q'"
+      if self.last_guess
+        puts "Your last guess was #{self.last_guess} and that's too damn #{self.low_or_high}"
+      else
+        puts "Make your first guess (1-100)."
+      end
+      guess = gets.chomp
+    end
   end
 end
